063e5705dcd06c7481d6061efde14a21c59f202e
hlmDev <a7m.almu@gmail.com>
2024-12-17 00:40:16 +0300

    Initial Commit

---
+++ muqlla/muqllaApp.swift
@@ -0,0 +1,17 @@
+//
+//  muqllaApp.swift
+//  muqlla
+//
+//  Created by Ahlam Majed on 17/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct muqllaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ muqllaTests/muqllaTests.swift
@@ -0,0 +1,17 @@
+//
+//  muqllaTests.swift
+//  muqllaTests
+//
+//  Created by Ahlam Majed on 17/12/2024.
+//
+
+import Testing
+@testable import muqlla
+
+struct muqllaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
