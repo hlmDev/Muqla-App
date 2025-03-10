ba88f29e538a3b4e509e9ccd728049ff72136336
Mohammad Alturbaq <MTurbaq.753@gmail.com>
2025-03-10 13:29:07 +0300

    Add supabase dependencies

---
+++ muqlla/core/Supabase/Supabase.swift.swift
@@ -0,0 +1,14 @@
+//
+//  Supabase.swift.swift
+//  muqlla
+//
+//  Created by Mohammad Alturbaq on 10/03/2025.
+//
+
+import Foundation
+import Supabase
+
+let supabase = SupabaseClient(
+  supabaseURL: URL(string: "https://phzdzfselzgmahehkyoe.supabase.co")!,
+  supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBoemR6ZnNlbHpnbWFoZWhreW9lIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE1NDgyNTIsImV4cCI6MjA1NzEyNDI1Mn0.wMXtt0qKvLHHdWvw1jbYaz_ye076MgmKVBdnintxUEM"
+)
