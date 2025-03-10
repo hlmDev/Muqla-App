9cc730f91be532424c7a1b921481e1c4254488fe
Mohammad Alturbaq <MTurbaq.753@gmail.com>
2025-03-10 15:58:54 +0300

    * main-mohammad Added: -appdelegate

---
+++ muqlla/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  muqlla
+//
+//  Created by Mohammad Alturbaq on 10/03/2025.
+//
+
+import Foundation
+import SwiftUI
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        return true
+    }
+}
